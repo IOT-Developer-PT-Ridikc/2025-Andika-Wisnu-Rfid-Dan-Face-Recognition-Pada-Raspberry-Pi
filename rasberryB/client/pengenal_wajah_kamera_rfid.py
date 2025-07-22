b71aa5dab114f45cf003416d073e739e70d9907e
JANU HADI SUSILO <82740265+UnionID@users.noreply.github.com>
2025-07-22 07:58:54 +0700

    selesai

---
+++ rasberryB/client/pengenal_wajah_kamera_rfid.py
@@ -0,0 +1,225 @@
+import cv2
+import requests
+import socket
+import time
+from concurrent.futures import ThreadPoolExecutor
+import json
+import serial
+import serial.tools.list_ports
+from datetime import datetime
+from datetime import date
+import time
+import os
+import ast
+
+server_ip = ""
+nama_terdeteksi = ""
+id_terdeteksi = ""
+status = ""
+kode_rfid = ""
+
+# Mendapatkan folder saat ini
+current_directory = os.path.dirname(os.path.abspath(__file__))
+
+print("Tanggal :", str(date.today()))
+print("Folder saat ini:", current_directory)
+# rasberry gak bisa cari port otomatis
+port_serial = "/dev/ttyUSB0"
+# port_serial = "COM7"
+
+# Default config
+config_path = os.path.join(current_directory, "config.json")
+default_config = {"kamera": 0, "port_serial": port_serial, "baudrate": 9600}
+
+# Cek dan buat file config jika belum ada
+if not os.path.exists(config_path):
+    with open(config_path, "w") as file:
+        json.dump(default_config, file, indent=4)
+
+# Baca config
+with open(config_path, "r") as file:
+    config = json.load(file)
+
+# Open the camera
+cap = cv2.VideoCapture(config["kamera"])
+port_serial = config["port_serial"]
+baud_rate = config["baudrate"]
+
+
+# Check if the camera is opened successfully
+
+if not cap.isOpened():
+    while 1:
+        print("Error: Could not open camera.")
+        print("Periksa nomor kamera / sambungan kamera ... ")
+        time.sleep(1)
+else:
+    print("Camera opened successfully.")
+
+
+def proses_kirim_serial(pesan):
+    pesan = pesan + "\n"
+    global ser
+    if pesan == "inisiasi\n":
+        try:
+            print("Mencoba port ", port_serial)
+            ser = serial.Serial(port_serial, baud_rate, timeout=1, write_timeout=1)
+            print(f"Terhubung ke {port_serial} dengan baud rate {baud_rate}")
+
+            time.sleep(2)  # tunggu koneksi stabil
+        except serial.SerialException as e:
+            print(f"Gagal membuka port serial: {e}")
+            while 1:
+                pass
+
+    elif pesan == "get_sensor\n":
+        ser.write(pesan.encode())
+        print("Mengirim permintaan sensor:", pesan)
+
+    else:
+        ser.write(pesan.encode())
+        print("Mengirim pesan serial:", pesan.encode())
+
+
+def get_base_ip():
+    global server_ip
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(("8.8.8.8", 80))
+        server_ip = s.getsockname()[0]
+    except:
+        server_ip = "127.0.0.1"
+    finally:
+        s.close()
+    return ".".join(server_ip.split(".")[:-1])  # contoh: 192.168.1
+
+
+def cari_server_flask():
+    base_ip = get_base_ip()
+    kandidat_ip = [f"{base_ip}.{i}" for i in range(1, 255)]
+
+    def cek(server_ip):
+        try:
+            r = requests.get(f"http://{server_ip}:5000/cek", timeout=0.5)
+            if r.text.strip() == "SERVER_FLASK_OK":
+                print(f"🟢 Server Flask ditemukan: {server_ip}")
+                return server_ip
+        except:
+            return None
+
+    with ThreadPoolExecutor(max_workers=50) as ex:
+        hasil = ex.map(cek, kandidat_ip)
+
+    hasil_valid = [server_ip for server_ip in hasil if server_ip]
+    return hasil_valid[0] if hasil_valid else None
+
+
+def kamera_dan_rfid(server_ip, rfid):
+    ret, frame = cap.read()
+    if not ret:
+        print("❌ Tidak dapat menangkap frame")
+        return
+
+    _, img_encoded = cv2.imencode(".jpg", frame)
+
+    try:
+        files = {"frame": ("frame.jpg", img_encoded.tobytes(), "image/jpeg")}
+        data = {"text": rfid}  # RFID
+
+        r = requests.post(f"http://{server_ip}:5000/upload_frame", files=files, data=data, timeout=5)
+
+        if r.status_code != 200:
+            print("⚠️ Upload gagal:", r.status_code)
+        else:
+            print("✅ Gambar dan RFID terkirim")
+    except Exception as e:
+        print("❌ Gagal kirim:", e)
+        utama()
+
+
+
+# LOGIKA UTAMA
+def kirim_gambar(server_ip):
+    global id_terdeteksi, nama_terdeteksi, status, kode_rfid
+    while True:
+        
+        proses_kirim_serial("get_sensor")
+        kode_rfid = ser.readline().decode("utf-8", errors="ignore").strip()
+        kamera_dan_rfid(server_ip,kode_rfid)
+        if kode_rfid != "":
+            print("data : ")
+            print("rfid :", kode_rfid)
+            print("id:", id_terdeteksi)
+            print("nama:", nama_terdeteksi)
+            print("status : ", status)
+            while 1:
+                proses_kirim_serial("@ID:" + kode_rfid)
+                status = requests.get(f"http://{server_ip}:5000/status").text
+                if status != "Tidak Dikenal" and status != "":
+                    # manajemen pensan_status
+                    list_status = status.split(",")
+
+                    if status != "":
+                        if len(list_status) < 2:
+                            # hanya tamplilan
+                            proses_kirim_serial("@" + status)
+                        else:
+                            # kontrol dan tampilan
+                            if list_status[-1] == "0":
+                                proses_kirim_serial("#" + list_status[1])  # tanpa buzzer
+                            elif list_status[-1] == "1":
+                                proses_kirim_serial("!" + list_status[1])  # dengan buzzer
+                            elif list_status[-1] == "2":
+                                proses_kirim_serial("%" + list_status[1])  # dengan buzzer panjang
+                            time.sleep(3)
+                    kode_rfid = ""
+                    
+                    break
+
+        if kode_rfid == "":
+            status = requests.get(f"http://{server_ip}:5000/status").text
+            data = requests.get(f"http://{server_ip}:5000/id_nama").text
+            id_terdeteksi, nama_terdeteksi = ast.literal_eval(data)
+
+            print("data : ")
+            print("rfid :", kode_rfid)
+            print("id:", id_terdeteksi)
+            print("nama:", nama_terdeteksi)
+            print("status : ", status)
+
+            # manajemen pensan_status
+            list_status = status.split(",")
+
+            if status != "":
+                if len(list_status) < 2:
+                    # hanya tamplilan
+                    proses_kirim_serial("@" + status)
+                else:
+                    # kontrol dan tampilan
+                    if list_status[-1] == "0":
+                        proses_kirim_serial("#" + list_status[1])  # tanpa buzzer
+                    elif list_status[-1] == "1":
+                        proses_kirim_serial("!" + list_status[1])  # dengan buzzer
+                    time.sleep(3)
+
+
+def utama():
+    while(1):
+        print("🔍 Mencari server Flask...")
+        ip_server = cari_server_flask()
+        if ip_server:
+            proses_kirim_serial("@Kamera Aktif")
+            print("📡 Mengirim gambar ke:", ip_server)
+            kirim_gambar(ip_server)
+        else:
+            print("❌ Tidak ditemukan server Flask.")
+        time.sleep(1)
+
+        proses_kirim_serial("@Mencari Server ..")
+        
+if __name__ == "__main__":
+    proses_kirim_serial("inisiasi")
+    proses_kirim_serial("Loading Kamera")
+    ret, frame = cap.read()
+    utama()
+    
---
+++ rasberryB/esp32/esp32.ino
@@ -0,0 +1,237 @@
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+#include <Arduino.h>
+
+
+#define SS_PIN 5
+#define RST_PIN 15
+#define BUZZER_PIN 2
+#define RELAY_PIN 14
+
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+
+String data = "";
+String kode_rfid = "kosong";
+String last_rfid = "";
+String last_data = "";
+String last_data2 = "";
+String last_data3 = "";
+
+int timer_display = 0;
+int status_serial = 0;
+
+
+
+void debug(String message, int row = 0, int clear = 1) {
+  //Serial.println(message);
+  //tampilkan jika menggunakan lcd
+  if (clear == 1) {
+    lcd.clear();
+  }
+  lcd.setCursor(0, row);
+  lcd.print(message);
+}
+
+
+
+void lcd_i2c(String text = "", int kolom = 0, int baris = 0, int clear = 1) {
+  byte bar[8] = {
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+    B11111,
+  };
+  if (text == "") {
+    lcd.init();  //jika error pakai lcd.init();
+    lcd.backlight();
+    lcd.createChar(0, bar);
+    lcd.setCursor(0, 0);
+    lcd.print("Loading..");
+    for (int i = 0; i < 16; i++) {
+      lcd.setCursor(i, 1);
+      lcd.write(byte(0));
+      delay(100);
+    }
+    delay(50);
+    lcd.clear();
+  } else {
+    if (clear == 1) {
+      lcd.clear();
+    }
+    lcd.setCursor(kolom, baris);
+    lcd.print(text + "                ");
+  }
+}
+
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd_i2c();
+  SPI.begin();
+  rfid.PCD_Init();
+
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(RELAY_PIN, OUTPUT);
+  bunyikanBuzzer();
+  buka_pintu();
+  delay(1000);
+  tutup_pintu();
+
+
+  debug("AKTIF");
+}
+
+
+
+void cekSerialInput() {
+
+  if (Serial.available()) {
+    data = Serial.readStringUntil('\n');
+    status_serial = 1;
+  }
+
+  if (millis() - timer_display > 5000) {
+    if (status_serial == 1) {
+      debug("Terhubung");
+      last_data = "";
+      last_data2 = "";
+      last_data3 = "";
+    }
+  }
+
+  if (data.indexOf('#') != -1) {
+    // Serial.println("Karakter ! ditemukan.");
+    data.remove(0, 1);
+    if (data != last_data) {
+      last_data = data;
+      debug(data);
+      buka_pintu();
+      delay(3000);
+      tutup_pintu();
+    }
+  }
+
+
+  if (data.indexOf('!') != -1) {
+    // Serial.println("Karakter ! ditemukan.");
+    data.remove(0, 1);
+    if (data != last_data2) {
+      last_data2 = data;
+      
+      debug(data);
+      bunyikanBuzzer();
+      buka_pintu();
+      delay(3000);
+      tutup_pintu();
+    }
+  }
+
+
+  if (data.indexOf('%') != -1) {
+    // Serial.println("Karakter @ ditemukan.");
+    data.remove(0, 1);
+    debug(data);
+    if (data != last_data3) {
+      last_data3 = data;
+      bunyikanBuzzerPanjang();
+    }
+
+    timer_display = millis();
+    
+  }
+
+  if (data.indexOf('@') != -1) {
+    // Serial.println("Karakter @ ditemukan.");
+    data.remove(0, 1);
+    last_data = "";
+    last_data2 = "";
+    last_data3 = "";
+    debug(data);
+
+    if (data == "Kamera Aktif") {
+      bunyikanBuzzer();
+      delay(100);
+      bunyikanBuzzer();
+      delay(100);
+      bunyikanBuzzer();
+    }
+    timer_display = millis();
+
+  }
+}
+
+
+void cekKartuRFID() {
+  if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial()) {
+    // Ambil UID
+    String uidStr = "";
+    for (byte i = 0; i < rfid.uid.size; i++) {
+      if (rfid.uid.uidByte[i] < 0x10) uidStr += "0";
+      uidStr += String(rfid.uid.uidByte[i], HEX);
+    }
+    uidStr.toUpperCase();
+
+    // Cek apakah kartu baru (berbeda dari sebelumnya)
+    if (uidStr != last_rfid) {
+      last_rfid = uidStr;
+      kode_rfid = uidStr;
+      Serial.println(kode_rfid);  // kirim hanya sekali
+      debug("ID:" + kode_rfid);
+      timer_display = millis();
+    }
+
+
+    // Matikan komunikasi dengan kartu
+    // rfid.PICC_HaltA();
+    // rfid.PCD_StopCrypto1();
+  }
+  // Reset UID jika kartu sudah diangkat
+  else if (!rfid.PICC_IsNewCardPresent()) {
+    last_rfid = "";
+  }
+}
+
+
+
+void buka_pintu() {
+
+  digitalWrite(RELAY_PIN, LOW);  // Relay ON (buka pintu)
+  //Serial.println("Pintu terbuka 🚪🔓");
+  delay(100);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void tutup_pintu() {
+
+  digitalWrite(RELAY_PIN, HIGH);
+  // Serial.println("Pintu tertutup 🚪🔓");
+  delay(100);
+  SPI.begin();
+  rfid.PCD_Init();
+}
+
+void bunyikanBuzzer() {
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(300);
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
+void bunyikanBuzzerPanjang() {
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(3000);
+  digitalWrite(BUZZER_PIN, LOW);
+}
+
+
+void loop() {
+  cekKartuRFID();
+  cekSerialInput();  // Baca perintah dari Serial
+}
